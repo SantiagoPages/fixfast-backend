57d508648e5e3146a528081a255cd7773a76d85f
SantiagoPages <santi.pages@hotmail.com>
2025-11-12 23:35:28 -0300

    Initial commit

---
+++ src/test/java/com/fixfast/fixfast_backend/FixFastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixfast.fixfast_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixFastBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
